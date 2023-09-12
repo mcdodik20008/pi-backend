9a54119f8c9c828f2ee731a48d360c21c5a3816e
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2023-09-12 17:43:14 +0500

    feat: sql-init-data-file

---
+++ src/main/resources/db/data/09/12.sql
@@ -0,0 +1,8 @@
+--liquibase formatted sql
+--changeset author:kaspshitskii
+INSERT INTO book VALUES ('123', '123', '123', '123', '123', '123');
+INSERT INTO book VALUES ('124', '124', '124', '124', '124', '124');
+INSERT INTO book VALUES ('125', '125', '125', '125', '125', '125');
+INSERT INTO book VALUES ('126', '126', '126', '126', '126', '126');
+INSERT INTO book VALUES ('127', '127', '127', '127', '127', '127');
+INSERT INTO book VALUES ('128', '128', '128', '128', '128', '128');
