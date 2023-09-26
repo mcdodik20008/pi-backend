da8884fdd25f4ffccba7250b7ce642c0d905f080
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2023-09-26 20:58:00 +0500

    feat: начало разделения ролей

---
+++ src/main/java/pibackend/infrastructure/RequestLoggingFilter.java
@@ -0,0 +1,53 @@
+package pibackend.infrastructure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter("/*")
+@Component
+public class RequestLoggingFilter implements Filter {
+
+    @Autowired
+    private SecurityContext securityContext;
+
+    private ServletContext servletContext;
+
+    public void init(FilterConfig fConfig) {
+        this.servletContext = fConfig.getServletContext();
+        this.servletContext.log("RequestLoggingFilter initialized");
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        String clientAuthCode = req.getHeader("ДоговоримсяКакой");
+        String servletPath = req.getServletPath();
+        boolean loginOrLogout = servletPath.equals("login") || servletPath.equals("logout");
+        if (loginOrLogout) {
+            // ЧИЛИМ!!!! (Регаем катку в тарков)
+        } else if (clientAuthCode != null && !clientAuthCode.isBlank()) {
+            // Проверяем закончилась ли сессия
+            Boolean sessionIsValid = securityContext.userIsLogin(clientAuthCode);
+            if (!sessionIsValid) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Время сессии вышло, авторизируйтесь повторно");
+            }
+        } else {
+            // Если чел не логинился, то домой его!!!!
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Доступ запрешен. Авторизируйтесь");
+        }
+
+        // если мы дошли до сюда, то пользователю можно пытаться делать что-то большее...
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+        //we can close resources here
+    }
+
+}
---
+++ src/main/java/pibackend/infrastructure/SecurityContext.java
@@ -0,0 +1,46 @@
+package pibackend.infrastructure;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+@Component
+public class SecurityContext {
+
+    private final Map<String, Pair<LocalDate, SecurityProperties.User>> authUsers = new HashMap<>();
+
+    private static SecurityProperties.User currentUser;
+
+    public Boolean userIsLogin(String clientAuthCode) {
+        // Логинился ли у нас пользователь
+        if (authUsers.containsKey(clientAuthCode)) {
+            Pair<LocalDate, SecurityProperties.User> pair = authUsers.get(clientAuthCode);
+            Duration duration = Duration.between(pair.getFirst(), LocalDate.now());
+            if (duration.get(ChronoUnit.MINUTES) < 30) {
+                currentUser = pair.getSecond();
+                authUsers.replace(clientAuthCode, Pair.of(LocalDate.now(), pair.getSecond()));
+                return true;
+            }
+            authUsers.remove(clientAuthCode);
+        }
+        return false;
+    }
+
+    public void addLoginUser(String clientAuthCode, SecurityProperties.User user){
+        if (authUsers.containsKey(clientAuthCode)){
+            authUsers.put(clientAuthCode, Pair.of(LocalDate.now(), user));
+        } else{
+            authUsers.replace(clientAuthCode, Pair.of(LocalDate.now(), user));
+        }
+    }
+}
