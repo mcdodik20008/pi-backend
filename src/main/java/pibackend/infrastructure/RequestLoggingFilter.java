443fddd68542d653f876224bfbfbac6c93b540d0
Alexey <stud0000250583@study.utmn.ru>
2023-09-28 18:51:41 +0500

    misc: disable Servlet Filter (for now)

--- src/main/java/pibackend/infrastructure/RequestLoggingFilter.java
+++ src/main/java/pibackend/infrastructure/RequestLoggingFilter.java
@@ -32,15 +32,15 @@ public class RequestLoggingFilter implements Filter {
         boolean loginOrLogout = servletPath.equals("login") || servletPath.equals("logout");
         if (loginOrLogout || swagger) {
             // ЧИЛИМ!!!! (Регаем катку в тарков)
-        } else if (clientAuthCode != null && !clientAuthCode.isBlank()) {
-            // Проверяем закончилась ли сессия
-            Boolean sessionIsValid = securityContext.userIsLogin(clientAuthCode);
-            if (!sessionIsValid) {
-                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Время сессии вышло, авторизируйтесь повторно");
-            }
-        } else {
-            // Если чел не логинился, то домой его!!!!
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Доступ запрещен. Авторизируйтесь");
+//        } else if (clientAuthCode != null && !clientAuthCode.isBlank()) {
+//            // Проверяем закончилась ли сессия
+//            Boolean sessionIsValid = securityContext.userIsLogin(clientAuthCode);
+//            if (!sessionIsValid) {
+//                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Время сессии вышло, авторизируйтесь повторно");
+//            }
+//        } else {
+//            // Если чел не логинился, то домой его!!!!
+//            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Доступ запрещен. Авторизируйтесь");
         }
 
         // если мы дошли до сюда, то пользователю можно пытаться делать что-то большее...
