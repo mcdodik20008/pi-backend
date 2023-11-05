5e0809608a24985a59fc6834364cf826074736c4
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2023-11-05 18:36:42 +0500

    feat: null

--- src/main/java/pibackend/infrastructure/RequestLoggingFilter.java
+++ src/main/java/pibackend/infrastructure/RequestLoggingFilter.java
@@ -36,7 +36,7 @@ public class RequestLoggingFilter implements Filter {
         String servletPath = req.getServletPath();
         String method = req.getMethod();
         String clientAuthCode = req.getHeader("Auth-Token");
-        List<String> exceptionPaths = Arrays.asList("/currentUser", "/login", "/logout", "/user/registration", "/user/change_password");
+        List<String> exceptionPaths = Arrays.asList("/login", "/logout", "/user/registration", "/user/change_password");
         if (method.equals("OPTIONS") || servletPath.contains("/swagger-ui") || exceptionPaths.contains(servletPath) || servletPath.contains("v3")) {
             // ЧИЛИМ!!!! (Регаем катку в тарков)
         } else if (clientAuthCode != null && !clientAuthCode.isBlank()) {
